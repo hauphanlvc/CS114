ec34a19cc26c60efebbda14e7140a76dae5e32d5
hauphanlvc <hauphanlvc@gmail.com>
2021-04-22 16:11:44 +0700

    Update

--- tuan4.1/ChiaHetCho3.py
+++ tuan4.1/ChiaHetCho3.py
@@ -1,85 +1,38 @@
-import io,os,sys
-# # input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
-# sys.stdin = open("InputChia.txt","r")
-sys.stdout = open("OuputChia.txt","w")
-def Tinh(n):
-    # n = (input())
-    # n = str(int(n))
-    n = str(n)
-    Sum = 0 
-    for i in n:
-        Sum+=int(i)
+n = (input())
+n = str(int(n))
 
-    # return (int(n[0]))
-    if n[0]!='9':
+# Tinh tong so cac chu so
+Sum = 0 
+for i in n:
+    Sum+=int(i)
+
+# Tim 1 chu so lam cho m lon nhat ma chia het cho 3 , khac m 
+k = 0
+KT = True
+while k < len(n):
+        
+    if n[k]!='9': # nếu chữ số thứ k khác 9
         i = 9
-    # return (int(n[0]))
-    # return (Sum)
-        while i >= int(n[0])+1:
-            Sum= Sum - int(n[0]) + i
-            # return (Sum)
-            if Sum % 3 ==0:
-                # return (Sum)
-                return (str(i)+n[1:])
-                # exit()
-                return
-            else :
-                Sum = Sum - i + int(n[0])
+        while i >= int(n[k])+1: #duyệt từ 9 đến chữ số đang xét cộng 1 
+            Sum= Sum - int(n[k]) + i # tinh lai tong voi chu so i
+            if Sum % 3 ==0: # nếu tổng các chữ số của nó chia hết cho 3 thì xuất ra đáp án
+                print(n[:k]+str(i)+n[k+1:])
+                KT = False
+                exit()
+            else : # ngược lại thì thì lại tổng các chữ số ban đầu
+                Sum = Sum - i + int(n[k])
                 i-=1 
+        k+=1 # tìm đến chữ số tiếp theo
+    else : k+=1 # tăng đến chữ số tiếp theo
         
-    # return (Sum)
-    New_Sum = Sum
-    while New_Sum % 3 !=0:
-        New_Sum+=1
-    # return (New_Sum)
-    KhoangCach = New_Sum - Sum
-    # return (KhoangCach)
-    k = 0
-    KT = True
-    # if KhoangCach ==0 : KhoangCach = 3
-    while k < len(n):
-            
-        if n[k]!='9':
-            i = 9
-    # return (int(n[0]))
-    # return (Sum)
-            while i >= int(n[k])+1:
-                Sum= Sum - int(n[k]) + i
-            # return (Sum)
-                if Sum % 3 ==0:
-                    # return (Sum)
-                    return (n[:k]+str(i)+n[k+1:])
-                    KT = False
-                    return 
-                    # exit()
-                else :
-                    Sum = Sum - i + int(n[k])
-                    i-=1 
-            k+=1
-        else : k+=1
-            
-        
-    if KT == True:
-            KhoangCach = 3 
-            x = n[0:len(n)-1]
-            
-            return ( x+ str(9 - KhoangCach))
-def SoSanh(x,k):
-    x = str(x)
-    k = str(k)
-    Count = 0
-    for i in range(len(x)):
-        if x[i]!=k[i] : Count+=1
-    if Count==1 : 
-        return True
-        # print("Đúng yeahhh")
-    else : 
-        return False
-        # print("Sai rồi :((")
-
-for x in range(100000000):
-    
-    k = Tinh(x)
-    if SoSanh(x,k)== False :
-        print(x,k)    
-    
+# truong hop da duyet het 
+if KT == True:
+        KhoangCach = int(n) % 3
+        x = n[0:len(n)-1]
+        if KhoangCach == 0 :
+            KhoangCach=3
+        elif KhoangCach== 1:
+            KhoangCach =1 
+        else:
+            KhoangCach = 2
+        print( x+ str(int(n[-1])-KhoangCach))
---
+++ tuan4.1/ThoaNgao.py
@@ -0,0 +1,5 @@
+x = 9
+n = int(1e100)
+while x < n:
+    if x % 3 !=0 : print(1)
+    x = int(str(x)+'9')
